b3af7eb2bd749ad4880641097082b4344598235c
Mitinul <torabi9030@gmail.com>
2024-03-20 14:25:12 +0330

    Landing on emptiness fixed

--- mario/src/LevelMaker.lua
+++ mario/src/LevelMaker.lua
@@ -8,7 +8,7 @@
     cogden@cs50.harvard.edu
 ]]
 
-LevelMaker = Class{}
+LevelMaker = Class {}
 
 function LevelMaker.generate(width, height)
     local tiles = {}
@@ -16,7 +16,7 @@ function LevelMaker.generate(width, height)
     local objects = {}
 
     local tileID = TILE_ID_GROUND
-    
+
     -- whether we should draw our tiles with toppers
     local topper = true
     local tileset = math.random(20)
@@ -30,7 +30,7 @@ function LevelMaker.generate(width, height)
     -- column by column generation instead of row; sometimes better for platformers
     for x = 1, width do
         local tileID = TILE_ID_EMPTY
-        
+
         -- lay out the empty space
         for y = 1, 6 do
             table.insert(tiles[y],
@@ -38,7 +38,7 @@ function LevelMaker.generate(width, height)
         end
 
         -- chance to just be emptiness
-        if math.random(7) == 1 then
+        if math.random(7) == 1 and x ~= 1 then
             for y = 7, height do
                 table.insert(tiles[y],
                     Tile(x, y, tileID, nil, tileset, topperset))
@@ -57,7 +57,7 @@ function LevelMaker.generate(width, height)
             -- chance to generate a pillar
             if math.random(8) == 1 then
                 blockHeight = 2
-                
+
                 -- chance to generate bush on pillar
                 if math.random(8) == 1 then
                     table.insert(objects,
@@ -67,20 +67,20 @@ function LevelMaker.generate(width, height)
                             y = (4 - 1) * TILE_SIZE,
                             width = 16,
                             height = 16,
-                            
+
                             -- select random frame from bush_ids whitelist, then random row for variance
                             frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7,
                             collidable = false
                         }
                     )
                 end
-                
+
                 -- pillar tiles
                 tiles[5][x] = Tile(x, 5, tileID, topper, tileset, topperset)
                 tiles[6][x] = Tile(x, 6, tileID, nil, tileset, topperset)
                 tiles[7][x].topper = nil
-            
-            -- chance to generate bushes
+
+                -- chance to generate bushes
             elseif math.random(8) == 1 then
                 table.insert(objects,
                     GameObject {
@@ -115,13 +115,10 @@ function LevelMaker.generate(width, height)
 
                         -- collision function takes itself
                         onCollide = function(obj)
-
                             -- spawn a gem if we haven't already hit the block
                             if not obj.hit then
-
                                 -- chance to spawn gem, not guaranteed
                                 if math.random(5) == 1 then
-
                                     -- maintain reference so we can set it to nil
                                     local gem = GameObject {
                                         texture = 'gems',
@@ -140,10 +137,10 @@ function LevelMaker.generate(width, height)
                                             player.score = player.score + 100
                                         end
                                     }
-                                    
+
                                     -- make the gem move up from the block and play a sound
                                     Timer.tween(0.1, {
-                                        [gem] = {y = (blockHeight - 2) * TILE_SIZE}
+                                        [gem] = { y = (blockHeight - 2) * TILE_SIZE }
                                     })
                                     gSounds['powerup-reveal']:play()
 
@@ -163,6 +160,6 @@ function LevelMaker.generate(width, height)
 
     local map = TileMap(width, height)
     map.tiles = tiles
-    
+
     return GameLevel(entities, objects, map)
 end
